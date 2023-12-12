2d9fa3fa1412728e5cb40584268294d53a4c1a8c
Joseph Stewart <josephlikescode@gmail.com>
2023-12-11 21:14:17 -0800

    Implement bookshelf puzzle.

--- bookshelf-puzzle-arduino/src/main.cpp
+++ bookshelf-puzzle-arduino/src/main.cpp
@@ -1,15 +1,68 @@
 #include <Arduino.h>
 #include <HCSR04.h>
 
-UltraSonicDistanceSensor distanceSensor(2, 3);
+void lockDoor();
+void unlockDoor();
+bool isPuzzleSolved();
 
-void setup() {
+const int LOCK_PIN = A5;
+const int SENSOR_COUNT = 3;
+
+UltraSonicDistanceSensor distanceSensors[SENSOR_COUNT] = {
+  UltraSonicDistanceSensor(2, 3),
+  UltraSonicDistanceSensor(5, 6),
+  UltraSonicDistanceSensor(8, 9)
+};
+
+bool isRunning = true;
+
+void setup()
+{
   Serial.begin(9600);
+  pinMode(LOCK_PIN, OUTPUT);
+  lockDoor();
 }
 
-void loop() {
-  Serial.println("New arduino project!");
-  auto distance = distanceSensor.measureDistanceCm();
-  Serial.println(distance);
-  delay(1000);
+void loop()
+{
+  if (!isRunning)
+  {
+    return;
+  }
+
+
+  if (isPuzzleSolved())
+  {
+    unlockDoor();
+    isRunning = false;
+  }
+}
+
+void unlockDoor()
+{
+  Serial.println("Unlocking door");
+  digitalWrite(LOCK_PIN, HIGH);
+}
+
+void lockDoor()
+{
+  Serial.println("Locking door");
+  digitalWrite(LOCK_PIN, LOW);
+}
+
+// Returns true if all distance sensors have an object within 5cm
+bool isPuzzleSolved()
+{
+  for (auto i = 0; i < SENSOR_COUNT; i++)
+  {
+    auto sensor = distanceSensors[i];
+    auto distance = sensor.measureDistanceCm();
+
+    if (distance > 5 || distance <= 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
 }

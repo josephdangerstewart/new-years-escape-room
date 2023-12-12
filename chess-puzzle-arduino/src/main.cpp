c67a544d6904e4faba96514ae97248b72adced67
Joseph Stewart <josephlikescode@gmail.com>
2023-12-11 19:52:00 -0800

    Add test for solenoid lock.

--- chess-puzzle-arduino/src/main.cpp
+++ chess-puzzle-arduino/src/main.cpp
@@ -1,11 +1,26 @@
 #include <Arduino.h>
 
+const int LOCK_PIN = A5;
+void printMagnetSensor();
+
 void setup()
 {
   Serial.begin(9600);
+  pinMode(LOCK_PIN, OUTPUT);
 }
 
 void loop()
+{
+  printMagnetSensor();
+  Serial.println("Setting HIGH");
+  digitalWrite(LOCK_PIN, HIGH);
+  delay(5000);
+  Serial.println("Setting LOW");
+  digitalWrite(LOCK_PIN, LOW);
+  delay(5000);
+}
+
+void printMagnetSensor()
 {
   auto sensorValue = analogRead(A0);
 
@@ -16,6 +31,4 @@ void loop()
 
   Serial.print("voltage: ");
   Serial.println(voltage);
-
-  delay(500);
 }
